eb35057a3c5152a037df33a0e864e7fed1aed778
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-03-18 01:55:46 -0400

    Finished Project

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Classes
+﻿using System;
+
+namespace Classes
 {
+    public class Automobile
+    {
+        public string Brand { get; set; }
+        public string ModelName { get; set; }
+        public int ManufactureYear { get; set; }
+    }
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Automobile myCar = new Automobile();
+            
+            myCar.Brand = "Mustang";
+            myCar.ModelName = "Shelby GT350";
+            myCar.ManufactureYear = 2020;;
+            
+            Console.WriteLine("Car Details:");
+            Console.WriteLine($"Make: {myCar.Brand}");
+            Console.WriteLine($"Model: {myCar.ModelName}");
+            Console.WriteLine($"Year: {myCar.ManufactureYear}");
         }
     }
 }
+
